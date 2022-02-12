55cbe6919764092ecf1dc9633b37f4b0d7e13eda
aktivgo <AktivGo01062001@gmail.com>
2022-02-12 23:18:22 +0300

    initial commit

---
+++ task1/task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+
+void fill_vector(std::vector<int>, int, int);
+void print_vector(std::vector<int>);
+
+const int SIZE = 10;
+const int LEFT_BORDER = 100000;
+const int RIGHT_BORDER = 1000000;
+
+int main()
+{
+    srand(time(0));
+
+    std::vector<int> vector(SIZE);
+    fill_vector(vector, LEFT_BORDER, RIGHT_BORDER);
+    print_vector(vector);
+}
+
+void fill_vector(std::vector<int> vector, int left_border, int right_border) {
+    std::cout << vector.size() << std::endl;
+    for (size_t i = 0; i < vector.size(); i++) {
+        vector.push_back(rand() % right_border + left_border);
+        std::cout << vector[i] << std::endl;
+    }
+}
+
+void print_vector(std::vector<int> vector) {
+    std::cout << vector.size() << std::endl;
+    for (size_t i = 0; i < vector.size(); i++) {
+        std::cout << vector[i] << " " << std::endl;
+    }
+}
