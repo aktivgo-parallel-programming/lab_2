674d07ba13f45f9178eb1a76f8b731a81b810a60
aktivgo <AktivGo01062001@gmail.com>
2022-02-12 23:32:29 +0300

    fix fill and print vector

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <ctime>
 
-void fill_vector(std::vector<int>, int, int);
+void fill_vector(std::vector<int> &, int, int, int);
 void print_vector(std::vector<int>);
 
 const int SIZE = 10;
@@ -13,21 +13,18 @@ int main()
 {
     srand(time(0));
 
-    std::vector<int> vector(SIZE);
-    fill_vector(vector, LEFT_BORDER, RIGHT_BORDER);
+    std::vector<int> vector;
+    fill_vector(vector, SIZE, LEFT_BORDER, RIGHT_BORDER);
     print_vector(vector);
 }
 
-void fill_vector(std::vector<int> vector, int left_border, int right_border) {
-    std::cout << vector.size() << std::endl;
-    for (size_t i = 0; i < vector.size(); i++) {
+void fill_vector(std::vector<int> &vector, int size, int left_border, int right_border) {
+    for (size_t i = 0; i < size; i++) {
         vector.push_back(rand() % right_border + left_border);
-        std::cout << vector[i] << std::endl;
     }
 }
 
 void print_vector(std::vector<int> vector) {
-    std::cout << vector.size() << std::endl;
     for (size_t i = 0; i < vector.size(); i++) {
         std::cout << vector[i] << " " << std::endl;
     }
