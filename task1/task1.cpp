b76de68d2f5f7d21cc356c0c1ab73b303b5855fe
aktivgo <AktivGo01062001@gmail.com>
2022-02-13 21:42:54 +0300

    complete task 1

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 #include <vector>
 #include <ctime>
+#include <execution>
 
 void fill_vector(std::vector<int> &, int, int, int);
 void print_vector(std::vector<int>);
+int calculate_divisor_count(int);
+double calculate_search_divisor_count_time(std::vector<int>, std::vector<int> &);
 
-const int SIZE = 10;
+const int SIZE = 2000000;
 const int LEFT_BORDER = 100000;
 const int RIGHT_BORDER = 1000000;
 
@@ -15,7 +18,17 @@ int main()
 
     std::vector<int> vector;
     fill_vector(vector, SIZE, LEFT_BORDER, RIGHT_BORDER);
-    print_vector(vector);
+
+    /*print_vector(vector);
+    std::cout << std::endl;*/
+
+    std::vector<int> result(vector.size());
+    double time = calculate_search_divisor_count_time(vector, result);
+    std::cout << "Time to search divisor count: " << time << "s" << std::endl;
+
+    //print_vector(result);
+
+    return 0;
 }
 
 void fill_vector(std::vector<int> &vector, int size, int left_border, int right_border) {
@@ -26,6 +39,30 @@ void fill_vector(std::vector<int> &vector, int size, int left_border, int right_
 
 void print_vector(std::vector<int> vector) {
     for (size_t i = 0; i < vector.size(); i++) {
-        std::cout << vector[i] << " " << std::endl;
+        std::cout << vector[i] << " ";
     }
+}
+
+int calculate_divisor_count(int n) {
+    int count = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (i * i != n) {
+                count++;
+            }
+            count++;
+        }
+    }
+
+    return count;
+}
+
+double calculate_search_divisor_count_time(std::vector<int> vector, std::vector<int> &result)
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
+    std::transform(std::execution::par_unseq, vector.begin(), vector.end(), result.begin(), calculate_divisor_count);
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    return diff.count();
 }
